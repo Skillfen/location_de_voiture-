045f0b9191b873b27784de567f6c86a51a224068
Skillfen <141834514+Skillfen@users.noreply.github.com>
2025-02-28 10:38:39 +0000

    Update V_Login.php

--- View/V_Login.php
+++ View/V_Login.php
@@ -30,7 +30,7 @@
             <form>
               <div class="form-floating mb-3">
                 <input value="<?= $login ?>" type="text" class="form-control" id="floatingInput" placeholder="Utilisateur" name="User" required>
-                <label for="floatingInput">Utilisateur</label>
+                <label for="floatingInput">User</label>
                 <span style="color:red"><?= $erreurlogin ?></span>
                 
               </div>
